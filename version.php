19d769ef1d7cfcd9d9eb1da7644d2ee97c1fd9d1
AndresNico <nico_andres@gmx.de>
2020-07-21 12:09:55 +0200

    Insert testdata in lr_subjects and lr_module

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_lecrec';
 $plugin->release = '0.1.0';
-$plugin->version = 2020062307;
+$plugin->version = 2020062308;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_ALPHA;
