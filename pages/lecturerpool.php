67a860581c770d1e8ae86d7f6fd6782027c63d5c
mazen01010 <41298065+mazen01010@users.noreply.github.com>
2020-07-22 20:46:31 +0200

    update

--- pages/lecturerpool.php
+++ pages/lecturerpool.php
@@ -3,7 +3,7 @@
 require(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_login();
 
-include('../table.php');
+
 
 global $DB, $PAGE, $OUTPUT, $CFG, $USER;
 
@@ -23,15 +23,63 @@ if (has_capability('local/lecrec:manager', $context)) {
 
     echo $OUTPUT->header();
     echo $OUTPUT->heading('Pool of available lecturers');
+    echo '
+    <form action="lecturer_c_u_form.php" method="post">
+      <div class="form-row">
+        <div class="col">
+          <input type="submit" value="Add New Lecturer" id="addRow9" class="btn btn-danger mb-2 pull-right"/>
+        </div>
+      </div>
+    </form>
+    <br/>
+    ';
+    $table = new html_table();
+    $table->id = 'my-table';
+    $table->attributes['class'] = 'table table-sm';
 
-    $attributes = array("lastname", "firstname", "professional_activities", "subject_area");
-    $head = array("Lastname", "Firstname", "Professional Activities", "Subject Area");
-    $align = array("center", "center", "center", "center");
-    $equals = array("");
+    $sql = "SELECT le.id, firstname, lastname , professional_activities , subject_area , dg.company_name
+            FROM {lr_lecturer} AS le
+            LEFT OUTER JOIN {dg_company} AS dg ON le.dg_company_id = dg.id";
+    $records = $DB->get_records_sql($sql);
+    //$records = $DB->get_records("lr_lecturer");
+
+    $table->head = array('ID', 'First Name', 'Last Name', 'Professional Activities', 'Subject Area', 'Company');
+    $table->align = array('center', 'center', 'center', 'center', 'center', 'center');
+
+    $i = 0;
+    foreach ($records as $record) {
+
+        $row = new html_table_row();
+        $row->attributes['record_id'] = $record->id;
+
+        $cell1 = new html_table_cell();
+        $cell1->text = ++$i;
+
+        $cell2 = new html_table_cell();
+        $cell2->text = $record->firstname;
+
+        $cell3 = new html_table_cell();
+        $cell3->text = $record->lastname;
+
+        $cell4 = new html_table_cell();
+        $cell4->text = $record->professional_activities;
+
+        $cell5 = new html_table_cell();
+        $cell5->text = $record->subject_area;
+
+        $cell6 = new html_table_cell();
+        $cell6->text = $record->company_name;
 
 
-    getTable("lr_lecturer", $attributes, $head, $align, "", $equals);
-    echo $OUTPUT->footer();
+
+        $row->cells  = array($cell1, $cell2, $cell3, $cell4, $cell5, $cell6);
+
+        $table->rowclasses[$record->id] = '';
+        $table->data[]  = $row;
+    };
+
+
+    echo html_writer::table($table);
 } else {
     redirect($CFG->wwwroot);
 }
@@ -64,3 +112,10 @@ if (has_capability('local/lecrec:manager', $context)) {
         $input = filter_input_array(INPUT_GET);
     };
 </script>
+
+
+<?php
+
+echo $OUTPUT->footer();
+
+?>
