f5facece2ba0139fc3f163acf5dfe85e6672b888
Omar <omaaar90@gmail.com>
2020-07-25 19:51:40 +0200

    application_view.php created

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_lecrec';
 $plugin->release = '0.1.0';
-$plugin->version = 2020062312;
+$plugin->version = 2020062313;
 
 
 $plugin->maturity = MATURITY_ALPHA;
