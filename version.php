8e055be5b6c13f1cd2d77f92644cfcc393794ec0
mazen01010 <41298065+mazen01010@users.noreply.github.com>
2020-07-30 21:56:19 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_lecrec';
 $plugin->release = '0.1.0';
-$plugin->version = 2020062314;
+$plugin->version = 2020062315;
 
 
 $plugin->maturity = MATURITY_ALPHA;
